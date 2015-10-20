398bb5751108aab473d20770b7811345cf911502
abock <abock@usgs.gov>
2015-10-20 15:44:22 -0600

    modified files, work in ToolsUI

--- MWBM_aggregation_CONUS/ConusLevel_duplictor.py
+++ MWBM_aggregation_CONUS/ConusLevel_duplictor.py
@@ -16,16 +16,9 @@ xmltemplate.close()
 ncfiles = [f for f in listdir(ncmls) if isfile(join(ncmls,f))]
 ncfiles_dim = filter(lambda x:dim in x,ncfiles)
 
-newfile = open("D:/abock/Water_Balance/MetaData/MWBM_Thredds/MWBM_aggregation_CONUS/2_DatasetLevel/conus-"+dim+".ncml","a")
-newfile.writelines(xmlLines[0:5])
-newfile.writelines("        <netcdf location=GSD_AET-"+dim+".ncml enhance='"'true'"'/>\n")
-newfile.writelines("        <netcdf location=GSD_PET-"+dim+"ncml enhance='"'true'"'/>\n")
-newfile.writelines("        <netcdf location=GSD_PPT-"+dim+".ncml enhance='"'true'"'/>\n")
-newfile.writelines("        <netcdf location=GSD_RO-"+dim+".ncml enhance='"'true'"'/>\n")
-newfile.writelines("        <netcdf location=GSD_SOIL-"+dim+".ncml enhance='"'true'"'/>\n")
-newfile.writelines("        <netcdf location=GSD_SWE-"+dim+".ncml enhance='"'true'"'/>\n")
-newfile.writelines("        <netcdf location=GSD_TAVE-"+dim+".ncml enhance='"'true'"'/>\n")
-for i in range (0,len(ncfiles_dim)):
-    newfile.writelines("        <netcdf location="+ncfiles_dim[i]+" enhance='"'true'"'/>\n")
-newfile.writelines(xmlLines[6:8])
-newfile.close()
+##newfile = open("D:/abock/Water_Balance/MetaData/MWBM_Thredds/MWBM_aggregation_CONUS/2_DatasetLevel/conus-"+dim+".ncml","a")
+##newfile.writelines(xmlLines[0:5])
+##for i in range (0,len(ncfiles_dim)):
+##    newfile.writelines("        <netcdf location="+'"'+ncfiles_dim[i]+'"'+" enhance="+'"'+"true"+'"'+"/>\n")
+##newfile.writelines(xmlLines[6:9])
+##newfile.close()
--- MWBM_aggregation_CONUS/VarLevel_duplictor.py
+++ MWBM_aggregation_CONUS/VarLevel_duplictor.py
@@ -1,13 +1,24 @@
 # This file writes complete metadata files for each GF region on the
 # MWBM database
-def varWriter (xLines,climateDS,var,DSdim,region):
-    newfile = open("d:/abock/Water_Balance/MetaData/MWBM_Thredds/MWBM_aggregation_CONUS/1_VarLevel/"+climateDS+"_"+var+"-"+DSdim+".ncml","w")
+def varWriter (xLines,climateDS,DSdim,region):
+    newfile = open("d:/abock/Water_Balance/MetaData/MWBM_Thredds/MWBM_aggregation_CONUS/1_VarLevel/"+climateDS+"-"+DSdim+".ncml","w")
     xmlLines_copy=list(xLines)
-    xmlLines_copy[2]=xmlLines_copy[2].replace("VAR",var)
-    xmlLines_copy[2]=xmlLines_copy[2].replace("DSname",climateDS+"_"+var)
+    xmlLines_copy[2]=xmlLines_copy[2].replace("DSname",climateDS)
     xmlLines_copy[2]=xmlLines_copy[2].replace("DSdim",DSdim)
+    xmlLines_copy[3]=xmlLines_copy[3].replace("DSname",climateDS)
+    xmlLines_copy[3]=xmlLines_copy[3].replace("DSdim",DSdim)
     xmlLines_copy[4]=xmlLines_copy[4].replace("DSname",climateDS)
     xmlLines_copy[4]=xmlLines_copy[4].replace("DSdim",DSdim)
+    xmlLines_copy[5]=xmlLines_copy[5].replace("DSname",climateDS)
+    xmlLines_copy[5]=xmlLines_copy[5].replace("DSdim",DSdim)
+    xmlLines_copy[6]=xmlLines_copy[6].replace("DSname",climateDS)
+    xmlLines_copy[6]=xmlLines_copy[6].replace("DSdim",DSdim)
+    xmlLines_copy[7]=xmlLines_copy[7].replace("DSname",climateDS)
+    xmlLines_copy[7]=xmlLines_copy[7].replace("DSdim",DSdim)
+    xmlLines_copy[8]=xmlLines_copy[8].replace("DSname",climateDS)
+    xmlLines_copy[8]=xmlLines_copy[8].replace("DSdim",DSdim)
+    xmlLines_copy[10]=xmlLines_copy[10].replace("DSname",climateDS)
+    xmlLines_copy[10]=xmlLines_copy[10].replace("DSdim",DSdim)
     newfile.writelines(xmlLines_copy)
     newfile.close()
     del xmlLines_copy
@@ -17,15 +28,18 @@ from os.path import isfile, join
 
 region="r01"
 varList=["AET","PET","PPT","RO","SOIL","SWE","TAVE"]
-dim="byHRU"
+dim="bySEG"
 
 #1 create/read-in necessary files
-template="D:/abock/Water_Balance/MetaData/MWBM_Thredds/MWBM_aggregation_CONUS/VarLevelTemp.ncml"
+template="D:/abock/Water_Balance/MetaData/MWBM_Thredds/MWBM_aggregation_CONUS/VarLevelTemp_"+dim+".ncml"
 netCDFs="F:/Yeti/ncdf_scripts/"+region
 xmltemplate=open(template,"r")
 # open the XML template of one GCM
 xmlLines=xmltemplate.readlines()
 xmltemplate.close()
+# using suffix for ncml
+# http://www.unidata.ucar.edu/software/thredds/v4.3/tds/tutorial/NcML.htm
+
 
 # get all netCDF files in the directory
 ncfiles = [ f for f in listdir(netCDFs) if isfile(join(netCDFs,f)) ]
@@ -39,17 +53,5 @@ gcm_only=[f[12:-9] for f in bcsd_hru]
 #gcmStr=yohoho[6::8]
 
 for i in range(0,len(gcm_only)):
-    varWriter(xmlLines,gcm_only[i][11:],"AET","byHRU",region)
-    varWriter(xmlLines,gcm_only[i][11:],"PET","byHRU",region)
-    varWriter(xmlLines,gcm_only[i][11:],"PPT","byHRU",region)
-    varWriter(xmlLines,gcm_only[i][11:],"RO","byHRU",region)
-    varWriter(xmlLines,gcm_only[i][11:],"SOIL","byHRU",region)
-    varWriter(xmlLines,gcm_only[i][11:],"SWE","byHRU",region)
-    varWriter(xmlLines,gcm_only[i][11:],"TAVE","byHRU",region)
-    varWriter(xmlLines,gcm_only[i][11:],"AET","bySEG",region)
-    varWriter(xmlLines,gcm_only[i][11:],"PET","bySEG",region)
-    varWriter(xmlLines,gcm_only[i][11:],"PPT","bySEG",region)
-    varWriter(xmlLines,gcm_only[i][11:],"RO","bySEG",region)
-    varWriter(xmlLines,gcm_only[i][11:],"SOIL","bySEG",region)
-    varWriter(xmlLines,gcm_only[i][11:],"SWE","bySEG",region)
-    varWriter(xmlLines,gcm_only[i][11:],"TAVE","bySEG",region)
+    varWriter(xmlLines,gcm_only[i][11:],dim,region)
+
